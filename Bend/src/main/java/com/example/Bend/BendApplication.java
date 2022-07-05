d95528f9529034bae706d192601efca4ea82bb53
sudarshan.rp <sudarshan.rp@CLAP2579.prodapt.com>
2022-07-05 16:36:43 +0530

    backend

---
+++ Bend/src/main/java/com/example/Bend/BendApplication.java
@@ -0,0 +1,15 @@
+package com.example.Bend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example.controller")
+public class BendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BendApplication.class, args);
+	}
+
+}
---
+++ Bend/src/test/java/com/example/Bend/BendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
